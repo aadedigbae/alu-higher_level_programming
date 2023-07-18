db6e158bcd979e2a92e5397118bd5aad88300641
aadedigbae <a.adedigba@alustudent.com>
2023-07-18 08:05:04 -0700

    made changes to file 0

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    for i in my_list:
-        print(str.format("{0}", i), end="\n")
+    for item in my_list:
+        print("{:d}".format(item))
